24fc8f98a4416f5c9966fc564489e19f7e51878e
maaramirezpi <maaramirezpi@unal.edu.co>
2025-09-11 10:39:13 -0500

    add script

---
+++ anon-scrapper.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from tempfile import mkdtemp
+
+
+def get_default_chrome_options():
+	options = webdriver.ChromeOptions()
+	options.add_argument("--no-sandbox")
+	options.add_argument("--disable-dev-shm-usage")
+	options.add_argument("--disable-dev-tools")
+	options.add_argument("--no-zygote")
+	options.add_argument("--headless=new")
+	options.add_argument("--single-process")
+	options.add_argument(f"--user-data-dir={mkdtemp()}")
+	options.add_argument(f"--data-path={mkdtemp()}")
+	options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+	options.add_argument("--remote-debugging-pipe")
+	options.add_argument("--verbose")
+	options.add_argument("--log-path=/tmp")
+	#options.add_argument("--disable-gpu")
+
+	return options
+
+options = get_default_chrome_options()
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://insta-stories-viewer.com/es//")
+
+
+for i in range(5):  # repeat 5 times
+	story_thingy = driver.find_element(By.CLASS_NAME, "profile__stories-counter")
+	print(f"Iteration {i+1} story count {story_thingy.text} --- am")
+	# do something here
+	time.sleep(2)  # wait 3 seconds
+
+driver.quit()
+
+#time.sleep(10)
+
+#story_tab2 = driver.find_element(By.CLASS_NAME, "profile__stories-counter")
+##stories2 = story_tab2.find_elements(By.TAG_NAME, "li")
+##stories_by_class2 = story_tab2.find_elements(By.CLASS_NAME,"profile__tabs-media-item")
+
+#print(story_tab2.text)
+
