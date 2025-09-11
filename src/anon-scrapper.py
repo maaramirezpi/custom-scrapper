2ebd1d8475c8553753c830293258f84c0341d4e0
maaramirezpi <maaramirezpi@unal.edu.co>
2025-09-11 07:59:54 -0500

    some fixes

--- src/anon-scrapper.py
+++ src/anon-scrapper.py
@@ -1,25 +1,43 @@
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
+import logging
 
-def get_default_chrome_options():
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
 
-    return options
+    options.binary_location = "/opt/chrome/chrome-linux64/chrome"
+
+    service = Service(
+        executable_path="/opt/chrome-driver/chromedriver-linux64/chromedriver",
+        service_log_path="/tmp/chromedriver.log"
+    )
+
+    driver = webdriver.Chrome(
+        service=service,
+        options=options
+    )
+
+    return driver
 
 
 def handler(event, context):
-    options = get_default_chrome_options()
-    driver = webdriver.Chrome(options=options)
+    driver = get_driver()
 
     driver.get("https://insta-stories-viewer.com/es/cristiano/")
 
+    story_count = ""
     for i in range(5):  # repeat 5 times
         story_thingy = driver.find_element(By.CLASS_NAME, "profile__stories-counter")
-        print(f"Iteration {i+1} story count {story_thingy.text} ---")
+        logger.info(f"Iteration {i+1} story count {story_thingy.text} ---")
+        story_count = story_thingy.text
         # do something here
         time.sleep(2)  # wait 3 seconds
 
@@ -30,7 +48,7 @@ def handler(event, context):
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": 'Hello from AWS Lambda using Python' + sys.version + '!'
+        "body": f"Hello from AWS Lambda using Python {sys.version}! Story count is {story_count}"
     }
 
     return response
